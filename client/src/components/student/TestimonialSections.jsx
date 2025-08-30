f80af4d2b73433f3bf1e222ffc0855a67ffa0841
Omkar <omkardjagtap2@gmail.com>
2025-08-30 19:06:57 +0530

    First Commit

---
+++ client/src/components/student/TestimonialSections.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TestimonialSections() {
+    return (
+        <div><h1>TestimonialSections</h1></div>
+    )
+}
+
+export default TestimonialSections
---
+++ client/src/pages/educator/AddCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddCourse() {
+  return (
+    <div>
+      <h1>AddCourse</h1>
+    </div>
+  )
+}
+
+export default AddCourse
