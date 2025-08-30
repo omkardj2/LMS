0f40839817f653cd83aeef2ba033bb54aecffe70
Omkar <omkardjagtap2@gmail.com>
2025-08-30 19:06:57 +0530

    First Commit

---
+++ client/src/components/student/CourseSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CourseSection() {
+    return (
+        <div>
+            <h1>Courses Section</h1>
+        </div>
+    )
+}
+
+export default CourseSection
---
+++ client/src/pages/student/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/student/Navbar'
+import Hero from '../../components/student/Hero'
+
+function Home() {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
