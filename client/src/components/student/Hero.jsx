e5dcc4f46cac95ab22e28409d2d5e1041a809071
Omkar <omkardjagtap2@gmail.com>
2025-09-01 23:03:02 +0530

    Create Home Page

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 function Hero() {
     return (
@@ -9,6 +10,7 @@ function Hero() {
             <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>We bring together world-class instructors, interactive content, and a support community to help you achieve your personal and professional goals. </p>
 
             <p className='md:hidden text-gray-500 max-w-sm mx-auto'> We bring together world-class instructors to help you achieve your professional goals </p>      
+            <SearchBar/>
         </div>
     )
 }
--- client/src/components/student/TestimonialSections.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TestimonialSections() {
-    return (
-        <div><h1>TestimonialSections</h1></div>
-    )
-}
-
-export default TestimonialSections
