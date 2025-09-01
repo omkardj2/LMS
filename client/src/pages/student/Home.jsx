10db06413016cf81e495121388381c58f4273e27
Omkar <omkardjagtap2@gmail.com>
2025-09-01 23:03:02 +0530

    Create Home Page

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from '../../components/student/Navbar'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
+import CoursesSection from '../../components/student/CoursesSection'
+import TestimonialsSections from '../../components/student/TestimonialsSections'
+import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
+      <Companies/>
+      <CoursesSection/>
+      <TestimonialsSections/>
+      <CallToAction/>
+      <Footer/>
     </div>
   )
 }
